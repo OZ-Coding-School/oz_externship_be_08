f0dd0d7c7290f36794d115748ade984b6566b354
gunung-kim <unge001@naver.com>
2026-04-21 14:47:38 +0900

    🐛 fix      : exam thumbnail_img_url필드 CharField로 변경

--- apps/exams/models/exam_model.py
+++ apps/exams/models/exam_model.py
@@ -6,7 +6,7 @@ from apps.core.models import TimeStampModel
 class Exam(TimeStampModel):
     # subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    thumbnail_image_url = models.ImageField(max_length=255, default="default_img_url")
+    thumbnail_image_url = models.CharField(max_length=255, default="default_img_url")
 
     class Meta:
         db_table = "exams"
