74fb67f4cb98bf9fb7204379c8a74677944569bf
gunung-kim <unge001@naver.com>
2026-04-23 18:17:30 +0900

    ✨ feat     :기수 과정 모델 추가

--- apps/exams/models/exam_deployment_model.py
+++ apps/exams/models/exam_deployment_model.py
@@ -1,6 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
+from apps.posts.models.cohort import Cohort
 from apps.core.models import TimeStampModel
 
 from .exam_model import Exam
@@ -12,7 +12,7 @@ class ExamDeployment(TimeStampModel):
         ON = "activate", "활성화"
         OFF = "deactivate", "비활성화"
 
-    # cohort_id = models.ForeignKey(Cohort,on_delete=models.CASCADE)
+    cohort_id = models.ForeignKey(Cohort,on_delete=models.CASCADE)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     duration_time = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(99)], default=60)
     access_code = models.CharField(max_length=64, unique=True)
--- apps/exams/models/exam_model.py
+++ apps/exams/models/exam_model.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from apps.posts.models.subject import Subject
 from apps.core.models import TimeStampModel
 
 
 class Exam(TimeStampModel):
-    # subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     thumbnail_image_url = models.CharField(max_length=255, default="default_img_url")
 
